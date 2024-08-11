b6787d6e91716d7c9ca48721b48b2a814cae3368
dchaitu <chaitanyadokara@gmail.com>
2024-08-11 20:22:00 +0530

    Checklist functions improved

--- src/App.js
+++ src/App.js
@@ -1,51 +1,85 @@
 import './App.css';
 import Task from "./components/Task/task";
+import {Component} from "react";
 
 
-const App = () => {
-  const tasks = [
-    {
-    "name":"Task 1",
-      "title":"Task One",
-      "status": "In Progress",
-      "priority": "Medium",
-  },
-    {
-    "name":"Task 2",
-      "title":"Task Two",
-      "status": "Yet to Start",
-      "priority": "High",
-  },
-  ]
+class App extends Component {
+    state = {
+        selectedTaskKey: null,
 
-  const getTasks = () => tasks.map(task => (
-          <Task taskName={task["name"]} taskTitle={task["title"]} taskStatus={task["status"]} taskPriority={task["priority"]}/>
-        ))
+    };
+    tasks = [
+        {
+            "id": 1,
+            "name": "Task 1",
+            "title": "Task One",
+            "status": "In Progress",
+            "priority": "Medium",
+        },
+        {
+            "id": 2,
+            "name": "Task 2",
+            "title": "Task Two",
+            "status": "Yet to Start",
+            "priority": "High",
+        },
+    ]
+    getSelectedTask = (taskId) => {
+        // console.log(this.state.isIssueSelected);
+        this.setState((prevState) => ({
+            selectedTaskKey: prevState.selectedTaskKey===taskId ? null: taskId,
+        }));
+    };
 
-  return (
-    <div>
-      <div className="container mx-auto px-4 sm:px-8">
-      <div className="min-w-full shadow rounded-lg overflow-hidden">
-      <table className="min-w-full leading-normal">
-        <thead>
-        <tr>
-            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                <input type="checkbox" className="form-checkbox"/>
-            </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Task</th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Title</th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Status</th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Priority</th>
-        </tr>
-        </thead>
-          <tbody>
-          {getTasks()}
-          </tbody>
-      </table>
-      </div>
-      </div>
-    </div>
-);
+
+    getTasks = () => this.tasks.map(task => {
+        const rowStyle = this.state.isIssueSelected ? {textDecoration: 'line-through'} : {};
+
+        return(
+        <Task styleName={rowStyle}
+              key={task.name}
+              taskName={task["name"]}
+              taskTitle={task["title"]}
+              taskStatus={task["status"]}
+              taskPriority={task["priority"]}
+              selectTask={() =>this.getSelectedTask(task["id"])}
+              isIssueSelected={this.state.isIssueSelected}
+        />)
+    })
+
+    render() {
+
+        return (
+            <div>
+            <div className="container mx-auto px-4 sm:px-8">
+            <div className="min-w-full shadow rounded-lg overflow-hidden">
+            <table className="min-w-full leading-normal">
+                <thead>
+                <tr>
+                    <TableRow>
+                        <input type="checkbox" className="form-checkbox"/>
+                    </TableRow>
+                    <TableRow>Task</TableRow>
+                    <TableRow>Title</TableRow>
+                    <TableRow>Status</TableRow>
+                    <TableRow>Priority</TableRow>
+                </tr>
+                </thead>
+                <tbody>
+                {this.getTasks()}
+                </tbody>
+            </table>
+        </div>
+            </div>
+            </div>
+        );
+    }
 }
+
+
+const TableRow = (props) => (
+    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">{props.children}</th>
+)
+
 
 export default App;
--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -1,16 +1,18 @@
 import './task.css'
 const Task = props => {
-  const {taskName, taskTitle, taskStatus, taskPriority} = props
-  return (
+  const {taskName, taskTitle, taskStatus, taskPriority,selectTask, isIssueSelected} = props
+    const styleName = isIssueSelected ? 'line-through' : '';
 
-      <tr>
+    return (
+
+      <tr className={styleName}>
           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-              <input type="checkbox" className="form-checkbox"/>
+              <input type="checkbox" className="form-checkbox" onClick={selectTask}/>
           </td>
-          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{taskName}</td>
-          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{taskTitle}</td>
-          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{taskStatus}</td>
-          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{taskPriority}</td>
+          <TableCell>{taskName}</TableCell>
+          <TableCell>{taskTitle}</TableCell>
+          <TableCell>{taskStatus}</TableCell>
+          <TableCell>{taskPriority}</TableCell>
       </tr>
 
 
@@ -18,3 +20,7 @@ const Task = props => {
 }
 
 export default Task
+
+const TableCell = (props) => (
+    <th className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{props.children}</th>
+)
