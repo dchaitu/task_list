d5c1ad90028b0f06c7e51b1e19b89a67e9f558f4
chaitanya <chaitanyadokara@gmail.com>
2024-08-16 10:03:16 +0530

    Adding Dropdrown Button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import './App.css';
 import Task from "./components/Task/task";
 import {Component} from "react";
 import Header from "./components/Header/header";
+import Dropdown from "./components/Dropdown/dropdown";
 
-
-class App extends Component {
-
-  state = {
-    isIssueSelected:''
-  }
-  taskStatus = {"YET_TO_START": "Yet To Start",}
-  tableStyle = "px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-  tasks = [
+const tasks = [
       {
         "id":"1",
         "name": "Task 1",
         "title": "Task One",
         "status": "In Progress",
         "priority": "Medium",
+        "tag": "Documentation"
       },
       {
         "id":"2",
@@ -25,43 +19,73 @@ class App extends Component {
         "title": "Task Two",
         "status": "Yet To Start",
         "priority": "High",
+        "tag": "Feature"
+      },
+      {
+        "id":"3",
+        "name": "Task 3",
+        "title": "Task Three",
+        "status": "Completed",
+        "priority": "High",
+        "tag": "Bug"
       },
     ]
+class App extends Component {
 
-  rowStyle = { textDecoration: 'line-through' };
+  state = {
+    isIssueSelected:null,
+    searchInput: '',
+    currentTasks: tasks,
+    isViewOpen:false
+  }
 
-  getSelectedTask = () => {
-    console.log("Clicked")
+
+
+  getSelectedTask = (taskId) => {
     this.setState((prevState) => ({
-      isIssueSelected: !prevState.isIssueSelected,
+      isIssueSelected: prevState.isIssueSelected === taskId ? null : taskId ,
     }));
   };
 
-  // selectedTask = (id) => {
-  //     for( task in this.tasks)
-  //     {
-  //       if( id === task["id"])
-  //         return
-  //       (<tr className={this.rowStyle}>
-  //           <Task taskName={task["name"]} taskTitle={task["title"]} taskStatus={task["status"]}
-  //                 taskPriority={task["priority"]}/>
-  //         </tr>
-  //       )
-  //     }
-  // }
+
+  getFilteredTasks = (titleName) =>  {
+    this.setState(
+      {currentTasks: tasks.filter((task)=> task["title"].includes(titleName))})
+      console.log("currentTasks:-",this.state.currentTasks);
+
+    }
 
 
 
-  getTasks = () => this.tasks.map(task => (
-    <Task taskId={task["id"]} taskName={task["name"]} taskTitle={task["title"]} taskStatus={task["status"]}
-          taskPriority={task["priority"]} selectTask={this.getSelectedTask}/>
-  ))
+  showDropDownMenu = () =>{
+    this.setState((prevState) => ({isViewOpen : !prevState.isViewOpen}))
+    console.log("isViewOpen:-", this.state.isViewOpen);
+  }
+
+  getTasks = () => this.state.currentTasks.map(task => {
+    const isIssueSelected = this.state.isIssueSelected === task["id"];
+    console.log(this.state.currentTasks);
+    console.log("isIssueSelected ", isIssueSelected, task["id"]);
+    return(
+    <Task taskTag={task["tag"]} taskId={task["id"]} taskName={task["name"]} taskTitle={task["title"]} taskStatus={task["status"]}
+          taskPriority={task["priority"]} selectTask={() => this.getSelectedTask(task["id"])}
+          isIssueSelected={isIssueSelected}
+    />
+  )})
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+    this.getFilteredTasks(this.state.searchInput);
+    //   console.log(this.state.searchInput);
+  }
 
 
   render() {
     return (
       <div>
-        <Header/>
+        <Header searchText={this.onChangeSearchInput} viewFilter={this.showDropDownMenu}/>
+        {this.state.isViewOpen && <Dropdown fieldOne="Title" fieldTwo="Status" fieldThree="Priority"/>}
         <div className="container mx-auto px-4 sm:px-8">
           <div className="min-w-full shadow rounded-lg overflow-hidden">
             <table className="min-w-full leading-normal">
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,25 @@
 import './header.css';
 
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { IoFilterOutline } from "react-icons/io5";
 
-const Header = () => (
-  <div className="container mx-auto px-4 sm:px-8">
+
+
+const Header = (props) => {
+
+  const {searchText, viewFilter,} = props;
+
+  return(<div className="container mx-auto px-4 sm:px-8">
     <h1 className="header">
       <b>Welcome back!</b></h1>
     <p>Here's a list of your tasks for this month!</p>
-  </div>
-)
+    <input className="text-box" onChange={searchText} type="text"/>
+    <button className="button" type='button'><AiOutlinePlusCircle className="inline-block"/>Status</button>
+    <button className="button" type='button'><AiOutlinePlusCircle className="inline-block"/>Priority</button>
+    <button className="button" type='button' onClick={viewFilter}><IoFilterOutline className="inline-block"/>View</button>
+
+
+  </div>)
+}
 
 export default Header
